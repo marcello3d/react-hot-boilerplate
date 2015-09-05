544085f5e4fe5b3dba7303004d505c57048aa8d1
Marcello Bastea-Forte <marcello@cellosoft.com>
2015-09-04 18:01:27 -0700

    migrate to actual handlebars for pages

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ var DEVELOPMENT_MODE = process.env.NODE_ENV === 'development'
 
 var express = require('express')
 var http = require('http')
-var path = require('path')
-var fs = require('fs')
 
 var webpackConfig = require('./webpack.config')
+
+var hbs = require('hbs')
 
 var app = express()
 
 var getAssetFilename = require('./webpack.middleware')(app)
-var sendFile = require('./fake-handlebars', getAssetFilename)
 
-// Expose two paths
-app.get('/', sendFile('index.html'))
-app.get('/page2', sendFile('page2.html'))
+app.set('view engine', 'hbs')
+hbs.registerHelper('asset_path', getAssetFilename)
+
+// Expose two pages
+app.get('/', function (req,res) {
+    res.render('index')
+})
+app.get('/page2', function (req,res) {
+    res.render('page2')
+})
 
 // Start the HTTP server
 var server = http.createServer(app)
