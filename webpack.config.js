a7884bb7cbaa0c8e37638b4fb201c281db7db4b0
Marcello Bastea-Forte <marcello@cellosoft.com>
2015-09-04 21:30:04 -0700

    update comments in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     devtool: IS_DEVELOPMENT_MODE ? 'cheap-module-eval-source-map' : 'source-map',
     entry: {
 
-        // Your JavaScript entry-points
+        // JavaScript entry-points
         index: './src/index',
         page2: './src/page2',
 
@@ -41,6 +41,8 @@ module.exports = {
 
         // Common plugins
         new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
+
+        // allows React to be minified properly in production
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(IS_DEVELOPMENT_MODE ? 'development' : 'production')
         })
@@ -48,25 +50,25 @@ module.exports = {
     ].concat(IS_DEVELOPMENT_MODE ? [
 
         // Dev-only plugins
-        new webpack.HotModuleReplacementPlugin(), // This generates delta patches for changing modules at runtime
+        new webpack.HotModuleReplacementPlugin() // generates delta patches for changing modules at runtime
 
     ] : [
 
         // Production-only plugins
         new webpack.optimize.OccurenceOrderPlugin(true),
-        new webpack.optimize.UglifyJsPlugin({ // This minifies/obfuscates your code
+        new webpack.optimize.UglifyJsPlugin({ // minifies your code
             compress: {
                 warnings: false
             }
         }),
-        new AssetsPlugin({path: path.join(__dirname, 'dist')})
+        new AssetsPlugin({path: path.join(__dirname, 'dist')}) // generates a json manifest of the hashed filepaths
     ]),
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
     module: {
         loaders: [{
-            // Transpile ES6/JSX code to ES5
+            // Transpile ES6 + JSX code to ES5
             test: /\.jsx?$/,
             loaders: ['babel'],
             include: path.join(__dirname, 'src')
